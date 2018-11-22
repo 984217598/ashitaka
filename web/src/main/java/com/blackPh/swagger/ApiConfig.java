497fce37e8f08d7b1641256e70400c3f1eab181b
BlackPh <984217598@qq.com>
2018-11-22 22:57:17 +0800

    swagger tomcat 插件构建整合

---
+++ web/src/main/java/com/blackPh/swagger/ApiConfig.java
@@ -0,0 +1,44 @@
+package com.blackPh.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *  
+ *  @author  wang.hao
+ *  @create 2018-11-22 22:43
+ **/
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = "com.blackPh")
+public class ApiConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.blackPh"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口列表 v1.1.0") // 任意，请稍微规范点
+                .description("接口测试") // 任意，请稍微规范点
+                .termsOfServiceUrl("http://localhost:8080/swagger-ui.html") // 将“url”换成自己的ip:port
+                .version("1.1.0")
+                .build();
+    }
+}

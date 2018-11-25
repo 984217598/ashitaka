7dddfe795cfaf44f11167753f7337505302abbec
王豪 <984217598@qq.com>
2018-11-25 15:47:54 +0800

    整合Swagger2

---
+++ web/src/main/java/com/blackPh/swagger/ApiConfig.java
@@ -0,0 +1,45 @@
+package com.blackPh.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author wang.hao
+ * @create 2018-11-25 下午3:37
+ **/
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = "com.blackPh")
+public class ApiConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.blackPh"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ashitaka 接口列表")
+                .description("ashitaka Api")
+                .termsOfServiceUrl("http://localhost:8080/swagger-ui.html")
+                .version("1.0.0")
+                .build();
+    }
+
+}

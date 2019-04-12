c4e00160c4ff38903d296354a0efe23dc606ae9d
BlackPh <984217598@qq.com>
2019-04-13 02:11:54 +0800

    兴蒂酒页面 demo1

--- web/src/main/java/com/blackPh/controller/xingdi/XingDiAction.java
+++ web/src/main/java/com/blackPh/controller/xingdi/XingDiAction.java
@@ -1,6 +1,8 @@
 package com.blackPh.controller.xingdi;
 
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @Api
 public class XingDiAction {
-
+    final static Logger LOGGER = LoggerFactory.getLogger(XingDiAction.class);
     @RequestMapping(value = "/xingdi", method = RequestMethod.GET)
     public String xingdi(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("aaaaaa");
+        LOGGER.info("aaaaaaaaaaaaaaaaaaa");
         return "xingdi";
     }
 
